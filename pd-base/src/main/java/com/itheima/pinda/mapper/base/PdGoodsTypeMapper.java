4a929168325e0f1409c83f5c469e39dfd75c966a
zt <960686470@qq.com>
2023-11-11 17:33:40 +0800

    feat: Add  Adding, deleting, modifying, and checking goods

--- pd-base/src/main/java/com/itheima/pinda/mapper/base/PdGoodsTypeMapper.java
+++ pd-base/src/main/java/com/itheima/pinda/mapper/base/PdGoodsTypeMapper.java
@@ -1,8 +1,12 @@
 package com.itheima.pinda.mapper.base;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.itheima.pinda.entity.base.PdGoodsType;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 /**
  * 天天进步
@@ -13,4 +17,12 @@ import org.apache.ibatis.annotations.Mapper;
  */
 @Mapper
 public interface PdGoodsTypeMapper extends BaseMapper<PdGoodsType> {
+
+
+    List<PdGoodsType> findByPage(
+            Page<PdGoodsType> page,
+            @Param("name")String name,
+            @Param("truckTypeId")String truckTypeId,
+            @Param("truckTypeName")String truckTypeName
+    );
 }
